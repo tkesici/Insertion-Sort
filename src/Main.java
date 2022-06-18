ed9c73a581bc182dcbf8bda9b8c32523542a77ea
tkesici <20200808004@ogr.akdeniz.edu.tr>
2022-06-18 20:16:00 +0300

    Added linear search algorithm

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,9 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-	int[] input = {31,41,59,26,41,58};
-        System.out.println("Result: " + Arrays.toString(insertionSort(input)));
+	int[] input = {6,1,3,4,2,5};
+        System.out.println("Result: " + Arrays.toString(insertionSortDescending(input)));
+        // System.out.println("Search: " + search(input,30));
     }
 
     public static int[] insertionSort(int[] sequence) {
@@ -27,5 +28,32 @@ public class Main {
             step++;
         }
         return sequence;
+    }
+
+    public static int[] insertionSortDescending(int[] sequence) {
+        int key = 0;
+        int i = 0;
+        int step = 1;
+        for (int j = 1; j < sequence.length ; j++) {
+            key = sequence[j]; // Operations run on the second element of the array
+            i = j-1; // The previous element that we will compare the element with key
+            while(i>=0 && sequence[i] <= key) {
+                sequence[i+1] = sequence[i]; // If any element before this element is greater than the key element, the elements are swapped
+                i = i-1;
+            }
+            sequence[i+1] = key;
+            System.out.println("Step " + step + ") Key: " + key + " Array: " + Arrays.toString(sequence));
+            step++;
+        }
+        return sequence;
+    }
+
+    public static java.lang.Integer search(int[] sequence, int v) {
+        for (int i = 0; i < sequence.length; i++) {
+            if (sequence[i] == v) {
+                return v;
+            }
+        }
+        return null;
     }
 }
